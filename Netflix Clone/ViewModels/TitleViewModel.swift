6789d54e3b5de2cb62493765b4f46810c634ad2b
RauanBimat <2016apolla@gmail.com>
2022-06-24 15:33:45 +0600

    add Upcoming View Contoller

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Рауан Абишев on 24.06.2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
